8005372bdbd4de869e0225fb61335763b58070ad
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-08-08 21:42:37 +0530

    add func

--- voting.sol
+++ voting.sol
@@ -13,4 +13,11 @@ contract ProposalContract {
     }
 
     mapping(uint256 => Proposal) proposal_history;
+
+    uint256 private counter;
+
+    function create(string calldata _description, uint256 _total_vote_to_end) external {
+        counter += 1;
+        proposal_history[counter] = Proposal(_description, 0, 0, 0, _total_vote_to_end, false, true);
+    }
 }
