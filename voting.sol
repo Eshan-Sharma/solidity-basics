00faa01b0df4bf2b629a51a6f61f9d71a2c053ba
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-08-08 22:14:54 +0530

    final version

--- voting.sol
+++ voting.sol
@@ -1,24 +1,29 @@
-//SPDX-License-Identifier:MIT
-pragma solidity ^0.8.19;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
 contract ProposalContract {
-    struct Proposal {
-        string description;
-        uint256 approve;
-        uint256 reject;
-        uint256 pass;
-        uint256 total_vote_to_end;
-        bool current_state;
-        bool is_active;
-    }
+    // ****************** Data ***********************
 
-    mapping(uint256 => Proposal) proposal_history;
+    //Owner
+    address owner;
 
     uint256 private counter;
 
-    address owner;
+    struct Proposal {
+        string description; // Description of the proposal
+        uint256 approve; // Number of approve votes
+        uint256 reject; // Number of reject votes
+        uint256 pass; // Number of pass votes
+        uint256 total_vote_to_end; // When the total votes in the proposal reaches this limit, proposal ends
+        bool current_state; // This shows the current state of the proposal, meaning whether if passes of fails
+        bool is_active; // This shows if others can vote to our contract
+    }
+
+    mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
+
     address[] private voted_addresses;
 
+    //constructor
     constructor() {
         owner = msg.sender;
         voted_addresses.push(msg.sender);
@@ -30,8 +35,19 @@ contract ProposalContract {
     }
 
     modifier active() {
-        require(proposal_history[counter].is_active == true, "The proposal is not active");
+        require(proposal_history[counter].is_active == true);
         _;
+    }
+
+    modifier newVoter(address _address) {
+        require(!isVoted(_address), "Address has not voted yet");
+        _;
+    }
+
+    // ****************** Execute Functions ***********************
+
+    function setOwner(address new_owner) external onlyOwner {
+        owner = new_owner;
     }
 
     function create(string calldata _description, uint256 _total_vote_to_end) external onlyOwner {
@@ -39,13 +55,11 @@ contract ProposalContract {
         proposal_history[counter] = Proposal(_description, 0, 0, 0, _total_vote_to_end, false, true);
     }
 
-    function setOwner(address new_owner) external onlyOwner {
-        owner = new_owner;
-    }
-
-    function vote(uint8 choice) external {
+    function vote(uint8 choice) external active newVoter(msg.sender) {
         Proposal storage proposal = proposal_history[counter];
         uint256 total_vote = proposal.approve + proposal.reject + proposal.pass;
+
+        voted_addresses.push(msg.sender);
 
         if (choice == 1) {
             proposal.approve += 1;
@@ -58,10 +72,14 @@ contract ProposalContract {
             proposal.current_state = calculateCurrentState();
         }
 
-        // Third part
         if ((proposal.total_vote_to_end - total_vote == 1) && (choice == 1 || choice == 2 || choice == 0)) {
             proposal.is_active = false;
+            voted_addresses = [owner];
         }
+    }
+
+    function terminateProposal() external onlyOwner active {
+        proposal_history[counter].is_active = false;
     }
 
     function calculateCurrentState() private view returns (bool) {
@@ -82,5 +100,24 @@ contract ProposalContract {
         } else {
             return false;
         }
+    }
+
+    // ****************** Query Functions ***********************
+
+    function isVoted(address _address) public view returns (bool) {
+        for (uint256 i = 0; i < voted_addresses.length; i++) {
+            if (voted_addresses[i] == _address) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function getCurrentProposal() external view returns (Proposal memory) {
+        return proposal_history[counter];
+    }
+
+    function getProposal(uint256 number) external view returns (Proposal memory) {
+        return proposal_history[number];
     }
 }
