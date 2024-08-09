58dc450ad926d9f559c80213ea29c4e140a02aae
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-08-09 07:55:49 +0530

    revision string and bytes

--- basics.sol
+++ basics.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.19;
 
 contract Basics {
     bool public isYes = false;
+
+    address public owner = address(0x01);
+    string public greet = "Hellow person";
+    bytes public helow = "Hellow Hellloowwww";
 }
